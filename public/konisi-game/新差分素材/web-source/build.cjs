d72671b37ca26b89f1b68f6335e4193b43e8065e
jimdelos100-afk <jimdelos100@gmail.com>
2026-09-23 21:20:39 +0800

    替换旧版网站为精简部署版

--- public/konisi-game/新差分素材/web-source/build.cjs
+++
@@ -1,8 +0,0 @@
-// Run: node build.cjs (Node.js required only for rebuilding, not for using the HTML.)
-const fs=require('fs');const path=require('path');
-const at=name=>path.join(__dirname,name);
-const output=fs.readFileSync(at('template.html'),'utf8')
- .replace('/*__ASSETS__*/',fs.readFileSync(at('assets-data.js'),'utf8'))
- .replace('/*__RUNTIME__*/',fs.readFileSync(at('runtime.js'),'utf8'));
-fs.writeFileSync(path.join(__dirname,'..','双击打开.html'),output);
-console.log('已生成：双击打开.html');
